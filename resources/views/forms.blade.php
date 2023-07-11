9ef18c79f0d77481d07df49c33a08d4f59bda26a
hersvin13 <87289544+hersvin13@users.noreply.github.com>
2023-07-11 11:52:48 +0800

    Update forms.blade.php

--- resources/views/forms.blade.php
+++ resources/views/forms.blade.php
@@ -4,19 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <form action="/employees" method="GET">
-        <input type="hidden" name="pass" value="12345">
-        <input type="text" name="personID" placeholder="Emp ID">
-        {{-- <input type="date" name="startDate">
-        <input type="time" name="startTime">
-        <input type="date" name="endDate">
-        <input type="time" name="endTime"> --}}
-        <input type="datetime-local" name="startTime" id="">
-        <input type="datetime-local" name="endTime" id="">
-        <input type="submit" value="Get all">
+
+    <form id="update-records" action="/employees" method="GET">
+    <h1>Update Records</h1>
+        <div class="row">
+            <div class="col-md-6">
+                    <h4>Employee</h4>
+                        <div class="class">
+                            <label>Get Name:</label>
+                            <label id="" name="empName">All Employee Name</label>
+                        </div>
+                        <div class="class">
+                            <label>Get Employee ID:</label>
+                            <label  id="" name="empId">All Employee ID</label>
+                        </div>
+                    
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                    <h4>Attendance</h4>
+                        <div class="class">
+                            <label id="" name="date">Date</label>
+                        </div>
+                        <div class="class">
+                            <label id="" name="startTime">Time In</label>
+                        </div>
+                        <div class="class">
+                            <label id="" name="endTime">Time Out</label>
+                        </div>
+            </div>
+        </div>
     </form>
 </body>
 </html>
